43ce6911f6ccf213f1adecef04357f872ffd0b7c
ywsfmkyn-netizen <his-actual-email@example.com>
2026-09-10 16:02:04 +0300

    edit

--- spi salve &Ram/spi_slave.v
+++ spi salve &Ram/spi_slave.v
@@ -80,10 +80,7 @@ always @(negedge sclk , negedge rst_n) begin
         tx_count <= 0 ;
     end
 
-    else if (~tx_valid) begin
-    tx_count <= 0;
-    end
-    else if (tx_count < 7) begin 
+    else if (tx_count < 8) begin 
     tx_count <= tx_count + 1 ; 
     end
 

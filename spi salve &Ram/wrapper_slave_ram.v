3eee71b6e6b68193ae74c6571c0ad1f530e7c135
ywsfmkyn-netizen <his-actual-email@example.com>
2026-09-10 12:45:11 +0300

    added wrapper_tb

--- spi salve &Ram/wrapper_slave_ram.v
+++ spi salve &Ram/wrapper_slave_ram.v
@@ -1,5 +1,9 @@
 module wrapper_slave_ram (sclk , rst_n , SS_n , MISO , MOSI);
 
+    parameter DATA_WIDTH = 8 ;
+    parameter MEM_DEPTH = 256 ;
+    parameter ADDR_WIDTH = 8 ;
+
     input sclk , rst_n , SS_n , MOSI ;
     output MISO ;
 
@@ -7,10 +11,6 @@ module wrapper_slave_ram (sclk , rst_n , SS_n , MISO , MOSI);
     wire [7:0] tx_data ;
     wire rx_valid ; 
     wire tx_valid ;
-
-    parameter DATA_WIDTH = 8 ;
-    parameter MEM_DEPTH = 256 ;
-    parameter ADDR_WIDTH = 8 ;
 
     spi_slave spi_slave1 (
         .sclk(sclk) , .rst_n(rst_n) , .SS_n(SS_n) , .MOSI(MOSI) , .MISO (MISO) , .rx_data(rx_data) , .tx_data(tx_data) , .rx_valid(rx_valid) , .tx_valid(tx_valid)

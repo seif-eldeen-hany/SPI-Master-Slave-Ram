965458e5bd2a474e2e43a1c402ecdd057308ea60
Seif <seifeldeen.eng@gmail.com>
2026-09-10 16:25:37 +0300

    seif

--- spi salve &Ram/spi_ram.v
+++ spi salve &Ram/spi_ram.v
@@ -17,8 +17,6 @@ module spi_ram (sclk , rst_n , rx_data , tx_data , rx_valid , tx_valid);
     reg [DATA_WIDTH-1:0] mem_array [0:MEM_DEPTH-1] ;
     reg [DATA_WIDTH-1:0] write_addr ;
     reg [DATA_WIDTH-1:0] read_addr ;
-    reg [3:0] tx_count ;
-    reg send_data_flag;
 
     integer i ;
 
@@ -31,32 +29,20 @@ always @(posedge sclk , negedge rst_n) begin
         tx_valid <= 0 ;
         write_addr <= 0 ;
         read_addr <= 0 ;
-        tx_count <= 0 ;
-        send_data_flag<= 0 ;
     end
     else begin
         if (rx_valid) begin
             case (rx_data [9:8])
                 OP_WRITE_ADDR : write_addr <= rx_data [ADDR_WIDTH-1:0] ;
                 OP_WRITE_DATA : mem_array[write_addr] <= rx_data [DATA_WIDTH-1:0] ;
-                OP_READ_ADDR : read_addr <= rx_data [ADDR_WIDTH-1:0] ;
-                OP_READ_DATA : begin
-                tx_data <= mem_array[read_addr] ;
-                send_data_flag<= 1 ;
-               end
-                default: tx_data <= 0 ;
+                OP_READ_ADDR : begin
+                                read_addr <= rx_data [ADDR_WIDTH-1:0] ;
+                                tx_data <= mem_array[rx_data [ADDR_WIDTH-1:0]] ;
+                                tx_valid <= 1 ;
+                                end
+                OP_READ_DATA : tx_valid <= 0 ;
+                default: tx_valid <= 0 ;
             endcase
-        end
-        if(send_data_flag)begin
-            if(tx_count<7)begin
-                tx_count <= tx_count + 1 ;
-                tx_valid <= 1 ;
-            end
-            else begin
-                send_data_flag <= 0 ;
-                tx_valid <= 0 ;
-                tx_count <= 0 ;     
-            end
         end
     end
 end    

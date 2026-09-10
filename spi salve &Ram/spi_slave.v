f8849befae341c44d7a4bdba5814731079178147
Seif <seifeldeen.eng@gmail.com>
2026-09-10 15:51:01 +0300

    dd

--- spi salve &Ram/spi_slave.v
+++ spi salve &Ram/spi_slave.v
@@ -80,16 +80,8 @@ always @(negedge sclk , negedge rst_n) begin
         tx_count <= 0 ;
     end
     
-<<<<<<< HEAD
     else if (rx_count > 2) begin
         tx_count <= tx_count + 1 ;
-=======
-    else if (~tx_valid) begin
-    tx_count <= 0;
-    end
-    else if (tx_count < 7) begin 
-    tx_count <= tx_count + 1 ; 
->>>>>>> 57e09f61fb6b879ff58df0dd89492b00391d905d
     end
 
     else begin

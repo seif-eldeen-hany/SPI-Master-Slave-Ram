07259e3236975979960cefd8e31f4ae33f220d98
ywsfmkyn-netizen <his-actual-email@example.com>
2026-09-10 12:29:58 +0300

    editting some errors

---
+++ spi salve &Ram/wrapper_slave_ram.v
@@ -0,0 +1,23 @@
+module wrapper_slave_ram (sclk , rst_n , SS_n , MISO , MOSI);
+
+    input sclk , rst_n , SS_n , MOSI ;
+    output MISO ;
+
+    wire [9:0] rx_data ;
+    wire [7:0] tx_data ;
+    wire rx_valid ; 
+    wire tx_valid ;
+
+    parameter DATA_WIDTH = 8 ;
+    parameter MEM_DEPTH = 256 ;
+    parameter ADDR_WIDTH = 8 ;
+
+    spi_slave spi_slave1 (
+        .sclk(sclk) , .rst_n(rst_n) , .SS_n(SS_n) , .MOSI(MOSI) , .MISO (MISO) , .rx_data(rx_data) , .tx_data(tx_data) , .rx_valid(rx_valid) , .tx_valid(tx_valid)
+    );
+    
+    spi_ram spi_ram1 (
+        .sclk(sclk) , .rst_n(rst_n) , .rx_data(rx_data) , .tx_data(tx_data) , .rx_valid(rx_valid) , .tx_valid(tx_valid)
+    );
+
+endmodule
